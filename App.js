3868968e3bece43bec7bb049ef9e5e3b240ea82c
Aaron Southammavong <aaroneight07@gmail.com>
2021-03-25 15:07:47 -0500

    removed unused code

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/screens/Home/Home';
@@ -11,11 +9,6 @@ const NavigationStack = createStackNavigator();
 
 const App = () => {
   return (
-    // <View style={styles.container}>
-    //   {/* <Text>Open up App.js to start working on your app!</Text> */}
-    //   <Home />
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>
       <NavigationStack.Navigator>
         <NavigationStack.Screen name="Home" component={Home} />
